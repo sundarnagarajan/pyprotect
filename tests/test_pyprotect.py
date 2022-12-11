91b615cea2047fc2483fa485966d71da9f40bf85
Sundar Nagarajan <sun.nagarajan@gmail.com>
2022-12-10 17:32:45 -0800

    Moved test_objects generator to separate module

--- tests/test_pyprotect.py
+++ tests/test_pyprotect.py
@@ -2,7 +2,6 @@
 
 import sys
 sys.dont_write_bytecode = True
-import re
 from math import ceil, floor, trunc
 import unittest
 import warnings
@@ -14,55 +13,11 @@ from test_utils import (
     check_predictions,
     MultiWrap,
 )
-from cls_gen import generate
 from pyprotect_finder import pyprotect    # noqa: F401
 from pyprotect import (
     freeze, private, protect, wrap,
 )
-
-
-def gen_test_objects():
-    '''generator'''
-    class C:
-        pass
-
-    class CI(int):
-        pass
-
-    class CF(float):
-        pass
-
-    cls_obj = generate(obj_derived=True)['class']
-    cls_obj_nested = generate(
-        obj_derived=True,
-        nested=True, depth=1000, no_cycles=False,
-    )['class']
-    cls_nonobj = generate(obj_derived=False)['class']
-    cls_nonobj_nested = generate(
-        obj_derived=False,
-        nested=True, depth=1000, no_cycles=False,
-    )['class']
-
-    l = [
-        1, [1, 2, 3], {'a': 1, 'b': 2},
-        C(),
-        CI, CF,
-        CI(10), CF(101.89),
-        cls_obj,
-        cls_nonobj,
-        cls_obj_nested,
-        cls_nonobj_nested,
-        cls_obj(),
-        cls_nonobj(),
-        cls_obj_nested(),
-        cls_nonobj_nested(),
-        re,
-    ]
-    for o in l:
-        yield o
-
-
-test_objects = gen_test_objects()
+from testcases import test_objects
 
 
 class test_pyprotect(unittest.TestCase):
---
+++ tests/testcases.py
@@ -0,0 +1,47 @@
+
+import re
+from cls_gen import generate
+
+
+def gen_test_objects():
+    '''generator'''
+    class C:
+        pass
+
+    class CI(int):
+        pass
+
+    class CF(float):
+        pass
+
+    cls_obj = generate(obj_derived=True)['class']
+    cls_obj_nested = generate(
+        obj_derived=True,
+        nested=True, depth=1000, no_cycles=False,
+    )['class']
+    cls_nonobj = generate(obj_derived=False)['class']
+    cls_nonobj_nested = generate(
+        obj_derived=False,
+        nested=True, depth=1000, no_cycles=False,
+    )['class']
+
+    l = [
+        1, [1, 2, 3], {'a': 1, 'b': 2},
+        C(),
+        CI, CF,
+        CI(10), CF(101.89),
+        cls_obj,
+        cls_nonobj,
+        cls_obj_nested,
+        cls_nonobj_nested,
+        cls_obj(),
+        cls_nonobj(),
+        cls_obj_nested(),
+        cls_nonobj_nested(),
+        re,
+    ]
+    for o in l:
+        yield o
+
+
+test_objects = gen_test_objects()
