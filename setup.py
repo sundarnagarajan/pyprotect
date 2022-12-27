5907507b9a66b4a848d2b5472bd04c51d0127d4d
Sundar Nagarajan <sun.nagarajan@gmail.com>
2022-12-27 13:47:11 -0800

    Document and make setup.py setup.cfg easier to reuse

--- setup.py
+++ setup.py
@@ -2,19 +2,26 @@ import sys
 sys.dont_write_bytecode = True
 from setuptools import setup, Extension
 
-version = '1.3'
 long_description = open('README.md', 'r').read(),
-
+version = '1.3'
+PY_MODULE = 'pyprotect'
+# Set EXT_Name = None if you don't have an extension
 # Do not require cython for INSTALLATION
 # Instead make sure DEVELOPER runs:
-#   'cython3 --3str protected.pyx' when(ever) protected.pyx changes
-ext_modules = [Extension(
-    'pyprotect.protected',
-    ['pyprotect/protected.c'],
-)]
+#   'cython3 --3str ${EXT_NAME}.pyx' when(ever) ${EXT_NAME}.pyx changes
+EXT_NAME = 'protected'
 
-setup(
+# ---------- Should not need to change anything after this ---------------
+ext_modules = []
+if EXT_NAME:
+    ext_modules = [Extension(
+        '%s.%s' % (PY_MODULE, EXT_NAME),
+        ['%s/%s.c' % (PY_MODULE, EXT_NAME)],
+    )]
+kwargs = dict(
     version=version,
-    packages=['pyprotect'],
-    ext_modules=ext_modules,
+    packages=[PY_MODULE],
 )
+if ext_modules:
+    kwargs['ext_modules'] = ext_modules
+setup(**kwargs)
