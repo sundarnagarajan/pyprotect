d0db1c21a73c1a2a6b5424d2bd715cbcae1c3766
Sundar Nagarajan <sun.nagarajan@gmail.com>
2023-01-12 15:48:26 -0800

    Added docker_image_must_exist, running_in_docker, must_be_in_docker, must_not_be_in_docker

--- scripts/common_functions.sh
+++ scripts/common_functions.sh
@@ -21,3 +21,33 @@ function hide_output_unless_error() {
     return 0
 }
 
+function docker_image_must_exist() {
+    # $1: image name
+    [[ $# -lt  1 ]] && {
+        >&2 red "Usage: docker_image_must_exist <image_name>"
+        return 1
+    }
+    docker image inspect "$1" 1>/dev/null 2>&1 || {
+        >&2 red "Docker image not found: $1"
+        return 1
+    }
+}
+
+function running_in_docker() {
+    # Returns: 0 if running in docker; 1 otherwise
+    grep -q '/init\.scope$' /proc/1/cgroup && return 1 || return 0
+}
+
+function must_be_in_docker() {
+    running_in_docker || {
+        >&2 red "Not running in docker"
+        return 1
+    } && return 0
+}
+
+function must_not_be_in_docker() {
+    running_in_docker && {
+        >&2 red "Running in docker, not on bare metal"
+        return 1
+    } || return 0
+}
