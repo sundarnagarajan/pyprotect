6118190eb8dc2f6b22967d32fee673fbab0659f9
Sundar Nagarajan <sun.nagarajan@gmail.com>
2023-01-12 12:03:43 -0800

    Use red function to highlight errors in red

--- scripts/test_in_docker.sh
+++ scripts/test_in_docker.sh
@@ -4,10 +4,18 @@
 set -e -u -o pipefail
 PROG_DIR=$(readlink -e $(dirname $0))
 source "$PROG_DIR"/config.sh
-DOCKER_USER="$(id -u):$(id -g)"
 
 # Script path from docker mount path perspective
 TEST_SCRIPT=${DOCKER_MOUNTPOINT}/tests/run_func_tests.sh
+
+function red() {
+    ANSI_ESC=$(printf '\033')
+    ANSI_RS="${ANSI_ESC}[0m"    # reset
+    ANSI_HC="${ANSI_ESC}[1m"    # hicolor
+    ANSI_FRED="${ANSI_ESC}[31m" # foreground red
+
+    echo -e "${ANSI_RS}${ANSI_HC}${ANSI_FRED}$@${ANSI_RS}"
+}
 
 [[ $# -lt 1 ]] && {
     PYVER='' 
@@ -17,14 +25,14 @@ TEST_SCRIPT=${DOCKER_MOUNTPOINT}/tests/run_func_tests.sh
 [[ "$PYVER" == "PY3" || "$PYVER" == "PY2" \
     || "$PYVER" == "PYPY3" || "$PYVER" == "PYPY2" || \
     "$PYVER" == "PYPY2" || -z "$PYVER" ]] || {
-    >&2 echo "Unknown PYVER: $PYVER"
+    >&2 red "Unknown PYVER: $PYVER"
     exit 1
 }
 
 cd "$PROG_DIR"/..
 [[ "$PYVER" == "PY3" || -z "$PYVER" ]] && {
     docker image inspect $PY3_DOCKER_IMAGE 1>/dev/null 2>&1 || {
-        >&2 echo "Docker image not found: $PY3_DOCKER_IMAGE"
+        >&2 red "Docker image not found: $PY3_DOCKER_IMAGE"
     } && {
         DOCKER_CMD="docker run --rm -v $(pwd):${DOCKER_MOUNTPOINT}:rw --user $DOCKER_USER $PY3_DOCKER_IMAGE ${TEST_SCRIPT} PY3"
         $DOCKER_CMD
@@ -32,7 +40,7 @@ cd "$PROG_DIR"/..
 }
 [[ "$PYVER" == "PY2" || -z "$PYVER" ]] && {
     docker image inspect $PY3_DOCKER_IMAGE 1>/dev/null 2>&1 || {
-        >&2 echo "Docker image not found: $PY2_DOCKER_IMAGE"
+        >&2 red "Docker image not found: $PY2_DOCKER_IMAGE"
     } && {
         DOCKER_CMD="docker run --rm -v $(pwd):${DOCKER_MOUNTPOINT}:rw --user $DOCKER_USER $PY2_DOCKER_IMAGE ${TEST_SCRIPT} PY2"
         $DOCKER_CMD
@@ -41,7 +49,7 @@ cd "$PROG_DIR"/..
 
 [[ "$PYVER" == "PYPY3" || -z "$PYVER" ]] && {
     docker image inspect $PYPY3_DOCKER_IMAGE 1>/dev/null 2>&1 || {
-        >&2 echo "Docker image not found: $PYPY3_DOCKER_IMAGE"
+        >&2 red "Docker image not found: $PYPY3_DOCKER_IMAGE"
     } && {
         DOCKER_CMD="docker run --rm -v $(pwd):${DOCKER_MOUNTPOINT}:rw --user $DOCKER_USER $PYPY3_DOCKER_IMAGE ${TEST_SCRIPT} PYPY3"
         $DOCKER_CMD
@@ -50,7 +58,7 @@ cd "$PROG_DIR"/..
 
 [[ "$PYVER" == "PYPY2" || -z "$PYVER" ]] && {
     docker image inspect $PYPY2_DOCKER_IMAGE 1>/dev/null 2>&1 || {
-        >&2 echo "Docker image not found: $PYPY2_DOCKER_IMAGE"
+        >&2 red "Docker image not found: $PYPY2_DOCKER_IMAGE"
     } && {
         DOCKER_CMD="docker run --rm -v $(pwd):${DOCKER_MOUNTPOINT}:rw --user $DOCKER_USER $PYPY2_DOCKER_IMAGE ${TEST_SCRIPT} PYPY2"
         $DOCKER_CMD
