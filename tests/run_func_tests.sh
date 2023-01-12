7247e3025a7dd1ed7dbb44d01429f6cdf5b35c68
Sundar Nagarajan <sun.nagarajan@gmail.com>
2023-01-12 12:02:07 -0800

    - Add function 'red' - Exit on error, even if running against multiple python versions - Use python version on command line even if in virtualenv

--- tests/run_func_tests.sh
+++ tests/run_func_tests.sh
@@ -1,25 +1,21 @@
 #!/bin/bash
-# $1: either PY2 or PY3 - defaults to testing in both
+# $1: PY3 | PY2 | PYPY3 | PYPY2 - defaults to testing in all
 
 PROG_DIR=$(readlink -e $(dirname $0))
 # We need $pwd in case we run it from Makefile dir (from Makefile)
 # export PYTHONPATH="$(readlink -e "${PROG_DIR}"):$(pwd):$PYTHONPATH"
 export PYTHONPATH=$(readlink -e "${PROG_DIR}")
 TEST_SCRIPT="${PROG_DIR}/test_pyprotect.py"
+set -eu -o pipefail
 
-env | grep -q '^VIRTUAL_ENV' && IN_VENV=yes || IN_VENV=no
-if [[ "$IN_VENV" = "yes" ]]; then
-    echo "Running in virtualenv"
-fi
-if [[ "$1" = "PY2" || "$1" = "PY3" || "$1" = "PYPY3" || "$1" = "PYPY2" ]]; then
-    PYVER=$1
-    shift
-else
-    PYVER=""
-fi
-REST_ARGS=$@
+function red() {
+    ANSI_ESC=$(printf '\033')
+    ANSI_RS="${ANSI_ESC}[0m"    # reset
+    ANSI_HC="${ANSI_ESC}[1m"    # hicolor
+    ANSI_FRED="${ANSI_ESC}[31m" # foreground red
 
-cd "${PROG_DIR}"
+    echo -e "${ANSI_RS}${ANSI_HC}${ANSI_FRED}$@${ANSI_RS}"
+}
 
 function test_in_1_python() {
     # $1: python command to use
@@ -27,10 +23,10 @@ function test_in_1_python() {
     local ret=0
     $PYTHON -B -c "from pyprotect_finder import pyprotect" 1>/dev/null 2>&1 || ret=1
     if [[ $ret -eq 0 ]]; then
-        echo "---------- Testing in $PYTHON ----------"
-        $PYTHON -B "${TEST_SCRIPT}" $REST_ARGS
+        echo "---------- Testing with $PYTHON ----------"
+        $PYTHON -B "${TEST_SCRIPT}"
     else
-        >&2 echo "$PYTHON module $MODULE_NAME not found"
+        >&2 red "$PYVER $PYTHON module pyprotect not found"
         return 1
     fi
 }
@@ -41,37 +37,61 @@ function run_all()
         local ret=0
         if [[ -z "$PYVER" || "$PYVER" = "PY3" ]]; then
             ret=0
-            test_in_1_python python3 || ret=1
-            if [[ $ret -ne 0 && -n "$PYVER" ]]; then
-                exit 1
-            fi
+            test_in_1_python python3
         fi
         if [[ -z "$PYVER" || "$PYVER" = "PY2" ]]; then
             ret=0
-            test_in_1_python python2 || ret=1
-            if [[ $ret -ne 0 && -n "$PYVER" ]]; then
-                exit 1
-            fi
+            test_in_1_python python2
         fi
         if [[ -z "$PYVER" || "$PYVER" = "PYPY3" ]]; then
         # if [[ "$PYVER" = "PYPY3" ]]; then
             ret=0
-            test_in_1_python pypy3 || ret=1
-            if [[ $ret -ne 0 && -n "$PYVER" ]]; then
-                exit 1
-            fi
+            test_in_1_python pypy3
         fi
         if [[ -z "$PYVER" || "$PYVER" = "PYPY2" ]]; then
         # if [[ "$PYVER" = "PYPY3" ]]; then
             ret=0
-            test_in_1_python pypy || ret=1
-            if [[ $ret -ne 0 && -n "$PYVER" ]]; then
-                exit 1
-            fi
+            test_in_1_python pypy
         fi
     else
-        test_in_1_python python
+        [[ -z "$PYVER" ]] && {
+            test_in_1_python python
+        } || {
+            case "$PYVER" in
+                PY3)
+                    test_in_1_python python3
+                    ;;
+                PY2)
+                    test_in_1_python python2
+                    ;;
+                PYPY3)
+                    test_in_1_python pypy3
+                    ;;
+                PYPY2)
+                    test_in_1_python pypy
+                    ;;
+                *)
+                    >&2 red "Unknown PYVER: $PYVER"
+                    return 1
+                    ;;
+            esac
+        }
     fi
 }
+
+env | grep -q '^VIRTUAL_ENV' && IN_VENV=yes || IN_VENV=no
+if [[ "$IN_VENV" = "yes" ]]; then
+    echo "Running in virtualenv"
+fi
+[[ $# -lt 1 ]] && PYVER="" || {
+    [[ "$1" = "PY2" || "$1" = "PY3" || "$1" = "PYPY3" || "$1" = "PYPY2" ]] || {
+        >&2 red "Unknown PYVER: $1"
+        exit 1
+    }
+    PYVER=$1
+    shift
+}
+
+cd "${PROG_DIR}"
 
 run_all
