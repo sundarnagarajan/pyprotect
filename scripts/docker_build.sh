8805df19b516ddd536f7d85e2e2dde79edff4942
Sundar Nagarajan <sun.nagarajan@gmail.com>
2023-01-13 22:56:15 -0800

    Support multiple Dockerfiles

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -35,23 +35,31 @@ do
         exit 1
     }
 done
-[[ -n $(declare -p TAG_IMAGE 2>/dev/null) ]] || {
-    >&2 red "Required variable not set in config.sh: TAG_IMAGE" 
-    exit 1
-}
-[[ -n "${TAG_IMAGE[PY3]}+_" ]] || {
-    >&2 red "Required variable not set in config.sh: TAG_IMAGE[PY3]"
+[[ -n $(declare -p DOCKERFILE_IMAGE 2>/dev/null) ]] || {
+    >&2 red "Required variable not set in config.sh: DOCKERFILE_IMAGE" 
     exit 1
 }
 
-
-IMAGE_NAME=${TAG_IMAGE[PY3]}
 cd "${PROG_DIR}"
-docker build ${ADDL_ARGS:-} \
-    --build-arg HOST_USERNAME=$HOST_USERNAME \
-    --build-arg HOST_GROUPNAME=$HOST_GROUPNAME \
-    --build-arg HOST_UID=$HOST_UID \
-    --build-arg HOST_GID=$HOST_GID \
-    --build-arg HOME_DIR=/home \
-    --build-arg PYPROTECT_DIR=/${PY_MODULE} \
-    -t $IMAGE_NAME .
+
+for k in ${!DOCKERFILE_IMAGE[@]}
+do
+    IMAGE_NAME=${DOCKERFILE_IMAGE[$k]}
+    [[ ${IMAGE_NAME}+x == "x" ]] && {
+        >&2 red "Image name not found for Docker file: $k"
+        exit 1
+    }
+    >&2 red "Building $IMAGE_NAME from $k"
+
+    docker build ${ADDL_ARGS:-} \
+        --build-arg HOST_USERNAME=$HOST_USERNAME \
+        --build-arg HOST_GROUPNAME=$HOST_GROUPNAME \
+        --build-arg HOST_UID=$HOST_UID \
+        --build-arg HOST_GID=$HOST_GID \
+        --build-arg HOME_DIR=/home \
+        --build-arg PYPROTECT_DIR=/${PY_MODULE} \
+        -t $IMAGE_NAME -f $k .
+done
+
+
+
