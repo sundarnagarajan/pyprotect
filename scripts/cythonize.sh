3e8a74cfe3bf008ffaa114a88f56d7662f98da0e
Sundar Nagarajan <sun.nagarajan@gmail.com>
2023-01-13 18:42:13 -0800

    Use "readlink -f" rather than "readlink -e" to be compatible with alpine linux busybox

--- scripts/cythonize.sh
+++ scripts/cythonize.sh
@@ -2,7 +2,7 @@
 # Can be fully reused, changing only config.sh
 #
 set -e -u -o pipefail
-PROG_DIR=$(readlink -e $(dirname $0))
+PROG_DIR=$(readlink -f $(dirname $0))
 SCRIPT_NAME=$(basename $0)
 source "$PROG_DIR"/config.sh
 source "$PROG_DIR"/common_functions.sh
