0d3fb98714ff1c230745b68a839c954121bad934
Sundar Nagarajan <sun.nagarajan@gmail.com>
2023-01-15 12:05:14 -0800

    Fix to scripts/update_signed_files.sh - do not sign signature.asc

--- scripts/update_signed_files.sh
+++ scripts/update_signed_files.sh
@@ -4,4 +4,5 @@ set -e -u -o pipefail
 PROG_DIR=$(dirname $0)
 cd "$PROG_DIR"/..
 echo "Updating signed_files.txt"
-git ls-files | LC_ALL=C sort > "${PROG_DIR}"/../signed_files.txt
+# Do not sign signature.asc
+git ls-files | fgrep -vx signature.asc | LC_ALL=C sort > "${PROG_DIR}"/../signed_files.txt
