1c641d536c09c392834bab8e6725add936a3e35a
Sundar Nagarajan <sun.nagarajan@gmail.com>
2023-01-17 14:39:16 -0800

    Add overrides / comments for CYTHON3_PROG_NAME and TAG_PYVER[PYTHON_VERSION]

--- scripts/config_docker_ubuntu.sh
+++ scripts/config_docker_ubuntu.sh
@@ -13,6 +13,18 @@
     CYTHON_DOCKER_FILE=Dockerfile.ubuntu
     # CYTHON3_DOCKER_IMAGE=$COMMON_CONTAINER_NAME
 
+    # Can OVERRIDE CYTHON3_PROG_NAME from default in cconfig.sh
+    # E.g. On Fedora 37, python3-Cython package installs v0.29.32 as 'cython'
+    # E.g. in Alpine Linux 3.15, cython package installs v0.29.24 as 'cython'
+    # E.g. on Arch, cython3 package installs v3.0.0a11 as 'cython'
+    # CYTHON3_PROG_NAME=cython
+
+    # Can selectively OVERRIDE python program executable basename for
+    # selected tags set in TAG_PYVER in config.sh
+    # E.g. on Fedora 37, default python3 is 3.11 and is not compatible
+    # with python3-Cython - need to install and use python3.10
+    # TAG_PYVER["PY3"]=python3.10
+
     # TAG_IMAGE: maps PYTHON_VERSION tags to docker image names
     declare -A TAG_IMAGE=(
         ["PY3"]=$COMMON_CONTAINER_NAME
