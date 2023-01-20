287aaa4ddf678495b39e7c15ad25ed5646e75120
Sundar Nagarajan <sun.nagarajan@gmail.com>
2023-01-19 19:20:44 -0800

    Use __NOTEST

--- scripts/host_install_test_in_docker.sh
+++ scripts/host_install_test_in_docker.sh
@@ -18,7 +18,7 @@ function install_test_1_pyver() {
     local pyver=$1
     local img=${TAG_IMAGE[${pyver}]}
     cd "$PROG_DIR"/..
-    DOCKER_CMD="docker run --rm -v $(pwd):${DOCKER_MOUNTPOINT}:rw --user root --env __DISTRO=$DISTRO --env __MINIMAL_TESTS=${__MINIMAL_TESTS:-} $img ${ROOT_SCRIPT} $pyver"
+    DOCKER_CMD="docker run --rm -it -v $(pwd):${DOCKER_MOUNTPOINT}:rw --user root --env __DISTRO=$DISTRO --env __MINIMAL_TESTS=${__MINIMAL_TESTS:-} --env __NOTEST=${__NOTEST:-} $img ${ROOT_SCRIPT} $pyver"
     $DOCKER_CMD
 }
 
@@ -27,7 +27,7 @@ cd "$PROG_DIR"/..
 [[ -n "${EXTENSION_NAME:-}" && "${CYTHONIZE_REQUIRED:-}" = "yes" ]] && {
     # Still need to check for CYTHON3_DOCKER_IMAGE and run CYTHONIZE_SCRIPT
     docker_image_must_exist $CYTHON3_DOCKER_IMAGE
-    DOCKER_CMD="docker run --rm -v $(pwd):${DOCKER_MOUNTPOINT}:rw --user "${HOST_UID}:${HOST_GID}" --env __DISTRO=${__DISTRO:-} $CYTHON3_DOCKER_IMAGE ${CYTHONIZE_SCRIPT}"
+    DOCKER_CMD="docker run --rm -it -v $(pwd):${DOCKER_MOUNTPOINT}:rw --user "${HOST_UID}:${HOST_GID}" --env __DISTRO=${__DISTRO:-} $CYTHON3_DOCKER_IMAGE ${CYTHONIZE_SCRIPT}"
     $DOCKER_CMD
 }
 
