78a05d85897285a9aaf3bbb5960e9ee35fcc05e0
Sundar Nagarajan <sun.nagarajan@gmail.com>
2023-01-25 18:40:46 -0800

    If we are showing (only) test outputs, also add a line showing what is being tested

--- r/scripts/run_func_tests.sh
+++ r/scripts/run_func_tests.sh
@@ -26,6 +26,8 @@ function test_in_1_pyver() {
             [[ $VERBOSITY -lt 1 ]] && {
                 hide_output_unless_error $python_cmd -B "${TEST_MODULE_FILENAME}" || return $?
             } || {
+                # If we are showing (only) test outputs, also add a line showing what is being tested
+                [[ $VERBOSITY -eq 1 ]] && echo "Testing for $python_cmd in $running_env"
                 $python_cmd -B "${TEST_MODULE_FILENAME}" || return $?
             }
         } || {
@@ -43,10 +45,11 @@ function test_in_1_pyver() {
 # ------------------------------------------------------------------------
 env | grep -q '^VIRTUAL_ENV' && IN_VENV=yes || IN_VENV=no
 [[ "$IN_VENV" = "yes" ]] && {
-    [[ $VERBOSITY -lt 2 ]] || echo "${SCRIPT_NAME}: Running in $(distro_name) as $(id -un) in virtualenv"
+    running_env="$(distro_name) as $(id -un) in virtualenv"
 } || {
-    [[ $VERBOSITY -lt 2 ]] || echo "${SCRIPT_NAME}: Running in $(distro_name) as $(id -un)"
+    running_env="$(distro_name) as $(id -un)"
 }
+[[ $VERBOSITY -lt 2 ]] || echo "${SCRIPT_NAME}: Running in $running_env"
 
 # If __TESTS_DIR env var is set, ONLY $TESTS_DIR/$TEST_MODULE_FILENAME is tried
 # Otherwise ONLY $PROG_DIR/../$TESTS_DIR/$TEST_MODULE_FILENAME is tried
