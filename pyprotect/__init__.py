a418be06e8fec35dacfb3d4a48c41c919feb47a3
Sundar Nagarajan <sun.nagarajan@gmail.com>
2023-01-27 18:05:32 -0800

    zip_safe is now future-proof

--- pyprotect/__init__.py
+++ pyprotect/__init__.py
@@ -1,3 +1,7 @@
+
+# -------------------------------------------------------------------------
+# Add additional __init__.py code AFTER this block
+# -------------------------------------------------------------------------
 import sys
 sys.dont_write_bytecode = True
 import os
@@ -8,44 +12,131 @@ DEFS_FILE_BASENAME = 'version_extname.ini'
 def get_ver_and_extmod():
     import sys
     import os
-    import sysconfig
-    import imp
-
-    if sys.version_info.major == 2:
-        from ConfigParser import ConfigParser
-    else:
-        from configparser import ConfigParser
+    from contextlib import contextmanager
 
     global UNKNOWN_VERSION
     global DEFS_FILE_BASENAME
     MODULE_DIR = os.path.dirname(os.path.realpath(__file__))
-    DEFS_FILE = os.path.join(MODULE_DIR, DEFS_FILE_BASENAME)
-    cfg = ConfigParser()
-    cfg.read(DEFS_FILE)
+    MODULE_NAME = os.path.basename(MODULE_DIR)
 
-    __ver = UNKNOWN_VERSION
-    ext_name = None
-    if cfg.has_option('defs', 'VERSION'):
-        __ver = cfg.get('defs', 'VERSION')
-    if cfg.has_option('defs', 'EXTENSION_NAME'):
-        ext_name = cfg.get('defs', 'EXTENSION_NAME')
+    class NoImportlibError(Exception):
+        pass
+
+    def cfg_get_ver_extname(cf):
+        '''
+        cf->str: path to ini file
+        Returns: (v->str, e-> str)
+        Small (import) difference between PY2 andPY3
+        '''
+        if sys.version_info.major == 2:
+            from ConfigParser import ConfigParser
+        else:
+            from configparser import ConfigParser
+        (_v, _e) = ('', '')
+        cfg = ConfigParser()
+        cfg.read(cf)
+        if cfg.has_option('defs', 'VERSION'):
+            _v = cfg.get('defs', 'VERSION')
+        if cfg.has_option('defs', 'EXTENSION_NAME'):
+            _e = cfg.get('defs', 'EXTENSION_NAME')
+        return(_v, _e)
+
+    def extension_suffix():
+        '''
+        Returns->str with leading dot
+        Small (CONFIG_KEY) difference between PY2 and PY3
+        '''
+        import sysconfig
+        if sys.version_info.major == 2:
+            CONFIG_KEY = "SO"
+        else:
+            CONFIG_KEY = 'EXT_SUFFIX'
+        return sysconfig.get_config_var(CONFIG_KEY)
+
+    def load_extension(name, path):
+        '''
+        name, path->str
+        Returns module object
+        PY2 has does not have importlib.util.spec_from_file_location
+        PY3 3 deprecates imp and will be REMOVED in 3.12
+        '''
+        if sys.version_info.major == 2:
+            import imp
+            return imp.load_dynamic(name, path)
+        else:
+            import importlib
+            spec = importlib.util.spec_from_file_location(name, path)
+            mod = importlib.util.module_from_spec(spec)
+            spec.loader.exec_module(mod)
+            return mod
+
+    @contextmanager
+    def using_file(f):
+        '''
+        f->str: Filename within a package
+        Use as context manager:
+            with using_file(my_package_file) as A:
+                do_something_with(A)
+        PY2 does not have importlib.resources
+        PY3 deprecates pkg_resources - may finally remove
+        '''
+        # Not using egg - NEEDED for running in-place
+        if os.path.exists(f):
+            try:
+                yield f
+            finally:
+                pass
+        else:
+            # Using egg
+            # Typically because zip_safe is True AND inside a virtualenv
+            # At least on Ubuntu, system-wide installs do not seem to use egg
+            # EVEN if zip_safe is True
+            try:
+                try:
+                    import importlib
+                    import importlib.resources
+                except ImportError:
+                    raise NoImportlibError
+                ref = importlib.resources.files(MODULE_NAME) / f
+                with importlib.resources.as_file(ref) as plf:
+                    try:
+                        yield str(plf)
+                    finally:
+                        pass
+            except NoImportlibError:
+                import pkg_resources
+                try:
+                    yield pkg_resources.resource_filename(
+                        MODULE_NAME,
+                        os.path.basename(f)
+                    )
+                finally:
+                    pkg_resources.cleanup_resources()
+            except:
+                import traceback
+                print(traceback.format_exc())
+                raise
+
+    # ---------- get_ver_and_extmod main code after this ----------
+    with using_file(DEFS_FILE_BASENAME) as f:
+        (__ver, ext_name) = cfg_get_ver_extname(f)
+        __ver = __ver or UNKNOWN_VERSION
 
     if not ext_name:
         return (__ver, None)
-
-    if sys.version_info.major == 2:
-        CONFIG_KEY = "SO"
-    else:
-        CONFIG_KEY = 'EXT_SUFFIX'
-    ext_fn = ext_name + sysconfig.get_config_var(CONFIG_KEY)
-    ext_fn = os.path.join(MODULE_DIR, ext_fn)
-    return (__ver, imp.load_dynamic(ext_name, ext_fn))
+    ext_fn = ext_name + extension_suffix()
+    with using_file(ext_fn) as f:
+        return (__ver, load_extension(ext_name, f))
 
 
 (__version__, e) = get_ver_and_extmod()
 if e:
     globals().update({k: getattr(e, k) for k in e.__all__})
     __doc__ = e.__doc__
-del sys, os, get_ver_and_extmod
-del UNKNOWN_VERSION, e
+del get_ver_and_extmod, e
+
+del sys, os
+del UNKNOWN_VERSION
+# -------------------------------------------------------------------------
+# Add additional __init__.py code AFTER this block
 # -------------------------------------------------------------------------
